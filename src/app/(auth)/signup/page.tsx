b08212310778ceaa75974d70470669772c6da65f
Wasif <wk5599426@gmail.com>
2024-03-25 22:14:04 +0500

    edited

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -17,23 +17,21 @@ const page = () => {
   // } = useForm({
   //   resolver: zodResolver(),
   // });
-  
+
   return (
     <>
       <div className="container relative flex pt-10 flex-col items-center justify-center lg:px-0">
-      <div className="ml-4 flex lg:ml-0">
-                <Link href={"/"}>
-                  <p className="text-5xl font-extrabold text-[#F3EDE4]">PIXELIZE</p>
-                </Link>
-          </div>
-          <h1 className="text-7xl font-extrabold pt-8">CREATE ACCOUNT</h1>
+        <div className="ml-4 flex lg:ml-0">
+          <Link href={"/"}>
+            <p className="text-5xl font-extrabold text-[#F3EDE4]">PIXELIZE</p>
+          </Link>
+        </div>
+        <h1 className="text-7xl font-extrabold pt-8">CREATE ACCOUNT</h1>
         <div className="mx-auto flex-w-full flex-col justify-center sm:w-[300px]">
           <div className="flex flex-col items-center space-y-2 text-center">
-            <Link
-              className='text-[#55DD4A] font-medium'
-              href="/signin"
-            >
-              Already have an account?<span className="hover:underline font-bold"> LogIn</span>
+            <Link className="text-[#55DD4A] font-medium" href="/signin">
+              Already have an account?
+              <span className="hover:underline font-bold"> LogIn</span>
             </Link>
           </div>
           <div className="grid gap-6">
